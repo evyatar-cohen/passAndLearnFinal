780de18428c1d68975af3b1afb442474af1ff2d4
evyatar-cohen <37417110+evyatar-cohen@users.noreply.github.com>
2019-02-11 23:00:13 +0200

    Add files via upload

---
+++ jScripts/myScript.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $(".about").click(function () {
+        $("#aboutDiv").toggle();
+    });
+
+    $(".howToPlay").click(function () {
+        $("#howToPlayDiv").toggle();
+    });
+
+    $(".closeAbout").click(function () {
+        $("#aboutDiv").hide();
+    });
+
+    $(".closeHowToPlay").click(function () {
+        $("#howToPlayDiv").hide();
+    });
+});
